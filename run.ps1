5e91cb2061fa7bb63b562a171537fd62484ef165
Felipe Torrezan <felipe.torrezan@iar.com>
2022-11-03 18:17:59 +0100

    Added run.ps1

---
+++ run.ps1
@@ -0,0 +1,41 @@
+# Powershell
+
+<#
+ # Copyright (c) 2022 IAR Systems AB
+ #
+ # run.ps1
+ #
+ # Run a container with the IAR Build Tools
+ # 
+ # See LICENSE for detailed license information
+ #>
+
+param(
+  [parameter(Mandatory=$true)]
+  # iarsystems/bx<package>:<version>
+  [String]$PkgNameVersion
+)
+
+$ScriptPath = Split-Path $PSCommandPath -Parent
+$PkgName = $PkgNameVersion.Split("/")[-1].Split(":")[0]
+$PkgVersion = $PkgNameVersion.Split(":")[-1]
+
+$DockerImageList = docker images --filter reference=iarsystems/$PkgName":"$PkgVersion -q --format "{{.Repository}}:{{.Tag}}"
+Write-Output $DockerImageList
+
+if ($DockerImageList -eq $null) {
+  Write-Output "ERROR: image $PkgName`:$PkgVersion not found."
+}
+
+Write-Output "-- Running a Docker container for $PkgName-$PkgVersion..."
+$CurrentLocation = Get-Location
+$ContainerID = docker run --detach `
+                          --hostname $env:ComputerName `
+                          --tty `
+                          --volume LMS2`:/usr/local/etc/IARSystems `
+                          --volume $CurrentLocation`:/build `
+                          $PkgNameVersion
+
+Write-Output "-- The working directory is $CurrentLocation."
+Write-Output " "
+Write-Output "-- Now execute the aliases-set.ps1 script."
