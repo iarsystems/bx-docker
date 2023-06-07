c31ddafcd5a52545f79b327084028a7ecd544830
Felipe Torrezan <felipe.torrezan@iar.com>
2023-06-07 17:40:01 +0200

    Updated PowerShell scripts
    - Fix #28

--- run.ps1
+++ run.ps1
@@ -10,33 +10,33 @@
 param(
   [parameter(Mandatory=$true)]
   # iarsystems/bx<package>:<version>
-  [String]$PkgNameVersion
+  [String]${PkgNameVersion}
 )
 
-$ScriptPath = Split-Path $PSCommandPath -Parent
-$PkgName = $PkgNameVersion.Split("/")[-1].Split(":")[0]
-$PkgVersion = $PkgNameVersion.Split(":")[-1]
+${ScriptPath} = Split-Path $PSCommandPath -Parent
+${PkgName} = ${PkgNameVersion}.Split("/")[-1].Split(":")[0]
+${PkgVersion} = ${PkgNameVersion}.Split(":")[-1]
 
-$DockerImageList = docker images --filter reference=iarsystems/$PkgName":"$PkgVersion -q --format "{{.Repository}}:{{.Tag}}"
-Write-Output $DockerImageList
+${DockerImageList} = docker images --filter reference=iarsystems/${PkgName}":"${PkgVersion} -q --format "{{.Repository}}:{{.Tag}}"
+Write-Output ${DockerImageList}
 
-if ($DockerImageList -eq $null) {
-  Write-Output "ERROR: image $PkgName`:$PkgVersion not found."
+if (${DockerImageList} -eq $null) {
+  Write-Output "ERROR: image ${PkgName}`:${PkgVersion} not found."
 }
 
-Write-Output "-- Running a Docker container for $PkgName-$PkgVersion..."
+Write-Output "-- Running a Docker container for ${PkgName}-${PkgVersion}..."
 $CurrentLocation = Get-Location
 $ContainerID = docker run --detach `
                           --hostname $env:ComputerName `
                           --tty `
                           --volume LMS2`:/usr/local/etc/IARSystems `
                           --volume $CurrentLocation`:/build `
-                          $PkgNameVersion
+                          ${PkgNameVersion}
 
 Write-Output "-- The working directory is $CurrentLocation."
 Write-Output " "
 
-$ImageAliases = "$ScriptPath\aliases-set.ps1"
+$ImageAliases = "${ScriptPath}\aliases-set.ps1"
 $args = @()
 $args += $ContainerID
 Invoke-Expression "$ImageAliases $args"
